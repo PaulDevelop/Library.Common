9f8aaa50c4b895994ec1d4967e7f0d9ea9351b9d
Rüdiger Scheumann <ruediger.scheumann@web.de>
2014-04-28 17:50:41 +0200

    * removed unused code

--- src/class/NodeCollection.php
+++ src/class/NodeCollection.php
@@ -2,8 +2,6 @@
 
 namespace Com\PaulDevelop\Library\Common;
 
-    //require_once 'INode.interface.php';
-
 /**
  * NodeCollection
  *
@@ -18,43 +16,4 @@ class NodeCollection extends GenericCollection
     {
         parent::__construct('Com\PaulDevelop\Library\Common\INode');
     }
-
-//    /**
-//     * @param string $offset
-//     *
-//     * @return INode
-//     */
-//    public function offsetGet($offset = null)
-//    {
-//        return parent::offsetGet($offset);
-//        //$result = nulll;
-//        //$result = isset($this->collection[$offset]) ? $this->collection[$offset] : null;
-//        //if ( $result != null ) {
-//        //    $result = $this->casttoclass('Com\PaulDevelop\Library\Common\INode', $result);
-//        //}
-//    }
-
-//    /**
-//     * Cast an object to another class, keeping the properties, but changing the methods
-//     *
-//     * @param string $class  Class name
-//     * @param object $object
-//     * @return object
-//     */
-//    function casttoclass($class, $object)
-//    {
-//        return unserialize(
-//            preg_replace('/^O:\d+:"[^"]++"/', 'O:' . strlen($class) . ':"' . $class . '"', serialize($object))
-//        );
-//    }
-
-//    /**
-//     * @param mixed $offset
-//     *
-//     * @return INode
-//     */
-//    public function offsetGet($offset = null)
-//    {
-//        return isset($this->collection[$offset]) ? $this->collection[$offset] : null;
-//    }
 }
