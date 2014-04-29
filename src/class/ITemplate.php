ca57434303208344578fb669a9e0cf5e9ac1685a
Rüdiger Scheumann <ruediger.scheumann@web.de>
2014-04-29 22:10:28 +0200

    * added interface ITemplate

---
+++ src/class/ITemplate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Com\PaulDevelop\Library\Common;
+
+/**
+ * ITemplate
+ *
+ * @package  Com\PaulDevelop\Library\Common
+ * @category Common
+ * @author   Rüdiger Scheumann <code@pauldevelop.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ */
+interface ITemplate
+{
+    /**
+     * @return string
+     */
+    public function process();
+}
