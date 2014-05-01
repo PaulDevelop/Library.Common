d04c05c4674add7a9baa6b1b3fda0cb3d4ef1557
Rüdiger Scheumann <ruediger.scheumann@web.de>
2014-05-01 15:01:56 +0200

    * added generic flag class

---
+++ src/class/GenericFlag.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Com\PaulDevelop\Library\Common;
+
+abstract class GenericFlag
+{
+    #region member
+    /**
+     * @var int
+     */
+    protected $flags;
+    /**
+     * @var string
+     */
+    private $type;
+    #endregion
+
+    #region constructor
+    public function __construct($type = 'unknown type')
+    {
+        $this->flags = null;
+
+        if (!class_exists($type)) {
+            throw new TypeCheckException('Type is not a class.');
+        }
+        $this->type = $type;
+    }
+    #endregion
+
+    #region methods
+    function addFlag($flag)
+    {
+        // check, if flag value is valid
+        $class = new \ReflectionClass ($this->type);
+        $constants = $class->getConstants();
+        $foundValue = false;
+        foreach ($constants as $value) {
+            if ($value == $flag) {
+                $foundValue = true;
+            }
+        }
+
+        if (!$foundValue) {
+            throw new \Exception('Flag not valid.');
+        }
+
+        $this->flags |= $flag;
+    }
+
+    function removeFlag($flag)
+    {
+        $this->flags &= ~$flag;
+    }
+
+    function check($flag)
+    {
+        if ($this->flags & $flag) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    #endregion
+}
